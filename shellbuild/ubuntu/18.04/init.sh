b4b25d1f7ae23fe3f0d26a7d1df5e8f3fe991d10
jason <jasonyang.final@gmail.com>
2020-09-15 12:37:15 +0900

    update

--- shellbuild/ubuntu/18.04/init.sh
+++ shellbuild/ubuntu/18.04/init.sh
@@ -47,14 +47,14 @@ wget -O "$PREFIX/$LIBMEM.tar.gz" https://launchpad.net/libmemcached/1.0/1.0.18/+
 mkdir -p "$PREFIX/$LIBMEM"
 tar xzvf "$PREFIX/$LIBMEM.tar.gz" -C "$PREFIX/$LIBMEM" --strip-components=1 && cd "$PREFIX/$LIBMEM"
 #we have to patch libmemcached on ubuntu 18.04
-wget -O "$LIBMEM.patch" https://src.fedoraproject.org/rpms/libmemcached/raw/HEAD/f/libmemcached-build.patch
+wget -O "$LIBMEM.patch" https://raw.githubusercontent.com/JasonYangShadow/fakechroot/master/shellbuild/ubuntu/18.04/libmemcached-build.patch
 patch ./clients/memflush.cc "$LIBMEM.patch"
 ./configure && make && sudo make install
 
 #step 3 download and compile msgpack locally
-wget -O "$PREFIX/$MSGPACK.zip" https://github.com/msgpack/msgpack-c/archive/master.zip
+wget -O "$PREFIX/$MSGPACK.tar.gz" https://github.com/msgpack/msgpack-c/releases/download/cpp-3.3.0/msgpack-3.3.0.tar.gz
 mkdir -p "$PREFIX/$MSGPACK"
-unzip "$PREFIX/$MSGPACK.zip" -d "$PREFIX/$MSGPACK"
-cd "$PREFIX/$MSGPACK/msgpack-c-master" && cmake . && make && sudo make install
+tar xzvf "$PREFIX/$MSGPACK.tar.gz" -C "$PREFIX/$MSGPACK"
+cd "$PREFIX/$MSGPACK/msgpack-3.3.0" && cmake . && make && sudo make install
 
 echo "Dependencies are installed and configured successfully!"
